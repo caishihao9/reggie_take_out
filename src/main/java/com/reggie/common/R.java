f7304357554a1f674b6832077b309e562a1af820
caishihao <2757583135@qq.com>
2022-09-28 16:44:13 +0800

    完善管理端订单信息功能

--- src/main/java/com/reggie/common/R.java
+++ src/main/java/com/reggie/common/R.java
@@ -1,5 +1,7 @@
 package com.reggie.common;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -11,15 +13,24 @@ import java.util.Map;
  * 通用返回结果，服务端响应的数据最后都会封装成此对象
  */
 @Data
+@ApiModel("返回结果")
 public class R<T> implements Serializable {
 
-    private Integer code; //编码：1成功，0和其它数字为失败
+    @ApiModelProperty("编码")
+    //编码：1成功，0和其它数字为失败
+    private Integer code;
 
-    private String msg; //错误信息
+    @ApiModelProperty("错误信息")
+    //错误信息
+    private String msg;
 
-    private T data; //数据
+    @ApiModelProperty("数据")
+    //数据
+    private T data;
 
-    private Map map = new HashMap(); //动态数据
+    @ApiModelProperty("动态数据")
+    //动态数据
+    private Map map = new HashMap();
 
     public static <T> R<T> success(T object) {
         R<T> r = new R<T>();
--- src/main/java/com/reggie/controller/OrderController.java
+++ src/main/java/com/reggie/controller/OrderController.java
@@ -1,14 +1,23 @@
 package com.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
+import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.reggie.common.R;
+import com.reggie.dto.OrdersDto;
 import com.reggie.entity.Orders;
+import com.reggie.entity.User;
 import com.reggie.service.OrderService;
+import com.reggie.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author caishihao
@@ -23,6 +32,14 @@ public class OrderController {
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 用户下单
+     * @param orders
+     * @return
+     */
     @PostMapping("/submit")
     public R<String> submit(@RequestBody Orders orders){
         log.info("订单信息：{}",orders);
@@ -30,5 +47,71 @@ public class OrderController {
         orderService.submit(orders);
 
         return R.success("用户下单成功");
+    }
+
+    /**
+     * 分页查询订单页面
+     * @param page
+     * @param pageSize
+     * @param number
+     * @param beginTime
+     * @param endTime
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize,String number
+                        ,String beginTime,String endTime){
+
+        log.info("beginTime:{}",beginTime);
+        log.info("endTime:{}",endTime);
+
+        //构造分页查询构造器
+        Page<Orders> pageInfo = new Page<>(page, pageSize);
+        Page<OrdersDto> orderDtoPage = new Page<>(page, pageSize);
+
+        //构建查询构造器
+        LambdaQueryWrapper<Orders> queryWrapper = new LambdaQueryWrapper<>();
+        queryWrapper.eq(StringUtils.hasLength(number),Orders::getId,number);
+        if (StringUtils.hasLength(beginTime) && StringUtils.hasLength(endTime)){
+            queryWrapper.between(Orders::getOrderTime,beginTime,endTime);
+        }
+        queryWrapper.orderByDesc(Orders::getCheckoutTime);
+        orderService.page(pageInfo,queryWrapper);
+
+        BeanUtils.copyProperties(pageInfo,orderDtoPage,"records");
+
+        List<Orders> records = pageInfo.getRecords();
+
+        List<OrdersDto> list = records.stream().map((item) -> {
+            OrdersDto ordersDto = new OrdersDto();
+            BeanUtils.copyProperties(item,ordersDto);
+            Long userId = item.getUserId();
+            User user = userService.getById(userId);
+            if (user != null){
+                ordersDto.setUserName(user.getName());
+            }
+            return ordersDto;
+        }).collect(Collectors.toList());
+
+        orderDtoPage.setRecords(list);
+
+        return R.success(orderDtoPage);
+    }
+
+    /**
+     * 修改订单状态信息
+     * @param orders
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Orders orders){
+        LambdaUpdateWrapper<Orders> updateWrapper = new LambdaUpdateWrapper<>();
+        //修改状态
+        updateWrapper.eq(Orders::getId,orders.getId());
+        updateWrapper.set(Orders::getStatus,orders.getStatus());
+
+        orderService.update(updateWrapper);
+        return R.success("修改状态成功");
+
     }
 }
